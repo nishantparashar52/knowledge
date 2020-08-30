f0fdf4987e485ea3bc59ecc89f43806993b34870
nishantp <root@Admins-MacBook-Pro-6.local>
2020-08-31 01:34:16 +0530

    added some methods

--- NodeJs_HTTP/app.js
+++ NodeJs_HTTP/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const { body, validationResult } = require('express-validator');
+const {
+    body,
+    validationResult
+} = require('express-validator');
 var cache = require('memory-cache');
 
 dotenv.config();
 app.use(express.json());
 
 
-//cache usage
-//data
+// Initial Data
 const course = [{
     name: 'nishant',
     class: 'B.Tech',
@@ -19,28 +21,30 @@ const course = [{
     name: 'ankit',
     class: 'BA',
     id: 2
-}];
-
+}
+];
 cache.put('course', course);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Hey Thanks for showing up');
 });
-app.get('/api/courses/:id', (req,res) => {
+app.get('/api/courses/:id', (req, res) => {
     let data;
-    if(!cache) {
-        data = course.find(item => item.id === parseInt(req.params.id));
-    } else data = cache.get('course').find(item => item.id === parseInt(req.params.id));
-    if(!data) return res.status('404').send('Error cannot get users');
+    data = getData(course, parseInt(req.params.id), cache);
+    if (!data) return res.status('404').send('Error cannot get users');
     res.send(data);
 });
 
 app.post('/api/courses', [
-    body('name').isLength({min: '3'}),
+    body('name').isLength({
+        min: '3'
+    }),
     body('class').exists()
-], (req,res) => {
+], (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) return res.status('400').send({errors: errors.array()});
+    if (!errors.isEmpty()) return res.status('400').send({
+        errors: errors.array()
+    });
     const reqCourse = {
         id: course.length + 1,
         name: req.body.name,
@@ -50,6 +54,43 @@ app.post('/api/courses', [
     cache.put('course', course);
     res.send(reqCourse);
 });
+
+// put
+app.put('/api/courses/:id', [
+    body('name').isLength({
+        min: '3'
+    }),
+    body('class').exists()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status('400').send({
+        errors: errors.array()
+    });
+    data = getData(course, parseInt(req.params.id), cache);
+    if (!data) return res.status('404').send('Error cannot set users data');
+    data['name'] = req.body.name;
+    data['class'] = req.body.class;
+    cache.put('course', course);
+    res.send(data);
+});
+
+// delete
+app.delete('/api/courses/:id', (req, res) => {
+    let data;
+    data = getData(course, parseInt(req.params.id), cache);
+    if (!data) return res.status('404').send('Error delete users');
+    const index = course.indexOf(data);
+    course.splice(index, 1);
+    cache.put('course', course);
+    res.send(data);
+});
+
+
+// Common function 
+const getData = (data, param, cache) => {
+    if (!cache) return course.find(item => item.id === param);
+    return cache.get('course').find(item => item.id === param);
+};
 const PORT = process.env.PO || 3000;
 
-app.listen(PORT, () =>console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
